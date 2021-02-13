660fb8e6100b6c9fb4d974ec86e13296112014b9
Bryan <windsor.bryan@gmail.com>
2021-02-13 21:18:48 +0100

    added header and joke components to app with bootstrap container

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "bootswatch/dist/darkly/bootstrap.min.css";
+import Header from "./Header";
+import { Container } from "react-bootstrap";
+import Favorites from "./Favorites";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container className="App">
+      <Header />
+      <Favorites />
+    </Container>
+  );
 }
 
 export default App;
