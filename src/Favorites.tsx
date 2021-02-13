a8d7347025fe7e6ad294782af0d315f954516518
Bryan <windsor.bryan@gmail.com>
2021-02-13 21:18:00 +0100

    created basic favorites list component

---
+++ src/Favorites.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+const Favorites = () => {
+  return (
+    <Row>
+      <Col className="text-center">
+        <h2>Favorites</h2>
+      </Col>
+    </Row>
+  );
+};
+
+export default Favorites;
