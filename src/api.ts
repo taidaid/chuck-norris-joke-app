2de77a7cc77a28f98a26dccc2caa0339c32847d7
Bryan <windsor.bryan@gmail.com>
2021-02-14 10:26:02 +0100

    - moved api file to correct place in folders - added return type to api call

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
+import { Joke } from "./interfaces";
+
 export const httpRequest = (url: string, options?: any) =>
   fetch(url, options)
     .then((response) => response.json())
     .catch((error) => console.error(error));
 
-export const get10Jokes = () =>
+export const get10Jokes = (): Promise<Joke[]> =>
   httpRequest("http://api.icndb.com/jokes/random/10");
